bd5790d81970dde1952c7075a82c74cca1f43f92
Oleksandr <sanyagolikov97@gmail.com>
2023-10-16 01:24:07 +0300

    Select image

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,8 +1,14 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:flutter/material.dart';
-import 'package:instagram_clone/utils/assets.dart';
+import 'dart:typed_data';
+
+import 'package:instagram_clone/providers/user_provider.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/models/user.dart';
+import 'package:instagram_clone/utils/utils.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -12,79 +18,133 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  final TextEditingController desctiption = TextEditingController();
+  Uint8List? file;
+
+  void postImage() async {
+    
+  }
+
+  _selectImage(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text('Create a post'),
+          children: [
+            SimpleDialogOption(
+              padding: EdgeInsets.all(20),
+              child: Text('Take a photo'),
+              onPressed: () async {
+                Navigator.of(context).pop();
+                Uint8List file = await pickImage(ImageSource.camera);
+                setState(() => this.file = file);
+              },
+            ),
+            SimpleDialogOption(
+              padding: EdgeInsets.all(20),
+              child: Text('Choose from galery'),
+              onPressed: () async {
+                Navigator.of(context).pop();
+                Uint8List file = await pickImage(ImageSource.gallery);
+                setState(() => this.file = file);
+              },
+            ),
+            SimpleDialogOption(
+              onPressed: () async => Navigator.of(context).pop(),
+              padding: EdgeInsets.all(20),
+              child: Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    desctiption.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    //   return Center(
-    //     child: IconButton(
-    //       icon: Icon(Icons.upload),
-    //       onPressed: () {},
-    //     ),
-    //   );
+    final User user = Provider.of<UserProvider>(context).getUser;
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.mobileBackground,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.arrow_back_ios_new_rounded,
-          ),
-        ),
-        title: Text('Post to'),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              'Post',
-              style: TextStyle(
-                color: AppColor.blue,
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-              ),
+    return file == null
+        ? Center(
+            child: IconButton(
+              onPressed: () => _selectImage(context),
+              icon: Icon(Icons.upload),
             ),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                backgroundImage: NetworkImage(AppAssets.avatar),
-              ),
-              SizedBox(
-                width: MediaQuery.sizeOf(context).width * .3,
-                child: TextField(
-                  maxLines: 8,
-                  decoration: InputDecoration(
-                    hintText: 'Write a caption...',
-                    border: InputBorder.none,
-                  ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: AppColor.mobileBackground,
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.arrow_back_ios_new_rounded,
                 ),
               ),
-              SizedBox(
-                height: 45,
-                width: 45,
-                child: AspectRatio(
-                  aspectRatio: 487 / 451,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(AppAssets.avatar),
-                        alignment: FractionalOffset.topCenter,
-                        fit: BoxFit.fill,
-                      ),
+              title: Text('Post to'),
+              actions: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Post',
+                    style: TextStyle(
+                      color: AppColor.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
                     ),
                   ),
                 ),
-              ),
-              const Divider(),          
-            ],
-          ),
-        ],
-      ),
-    );
+              ],
+            ),
+            body: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(backgroundImage: NetworkImage(user.photoUrl)),
+                    SizedBox(
+                      width: MediaQuery.sizeOf(context).width * .3,
+                      child: TextField(
+                        maxLines: 8,
+                        controller: desctiption,
+                        decoration: InputDecoration(
+                          hintText: 'Write a caption...',
+                          border: InputBorder.none,
+                          hintStyle: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.secondary,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 45,
+                      width: 45,
+                      child: AspectRatio(
+                        aspectRatio: 487 / 451,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              alignment: FractionalOffset.topCenter,
+                              image: MemoryImage(file!),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
   }
 }
